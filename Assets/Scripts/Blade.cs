071f95ee88dfdc1bac44323e7c5f46e090c3cabc
Toan Thang <box.dota@gmail.com>
2023-11-15 01:40:01 +0300

    Blade

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera mainCamera;
+    private Collider bladeCollider;
+    private bool slicing;
+
+    public Vector3 direction { get; private set; }
+    public float minSliceVelocity = 0.01f;
+
+    private void Awake()
     {
-        
+        mainCamera = Camera.main;
+        bladeCollider = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        StopSlicing();
+    }
+
+    private void OnDisable()
+    {
+        StopSlicing();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartSlicing();
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            StopSlicing();
+        } else if (slicing)
+        {
+            ContinueSlicing();
+        }
+    }
+
+    private void StartSlicing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        transform.position = newPosition;
+
+        slicing = true;
+        bladeCollider.enabled = true;
+    }
+
+    private void StopSlicing()
+    {
+        slicing = false;
+        bladeCollider.enabled = false;
+    }
+
+    private void ContinueSlicing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        direction = newPosition - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        bladeCollider.enabled = velocity > minSliceVelocity;
+
+        transform.position = newPosition;
     }
 }
